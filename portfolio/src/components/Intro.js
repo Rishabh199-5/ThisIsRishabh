93d1c85ab6e1fbcb46e81b7c30cefb606b0377da
Rishabh <rish@adobe.com>
2021-12-24 14:17:46 +0530

    introduction page

--- portfolio/src/components/Intro.js
+++ portfolio/src/components/Intro.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import IntroViewCSS from '../styles/Intro.module.css';
 import skyISTheLimitGIF from '../assets/skyISTheLimit.gif';
+import classnames from "classnames";
 
 const Intro = () => {
     const interests = [
-        "a web developer",
-        "a fitness enthusiast",
-        "a dancer",
-        "a proud Indian"
+        "a web developer .",
+        "a fitness enthusiast .",
+        "a dancer .",
+        "a proud Indian ."
     ]
+    const [visibleIndex,setVisibleIndex] = useState(0);
+    const [containerWidth,setContainerWidth] = useState('1.5em');
+
+    useEffect(() => {
+        let innerInterval;
+        let interval = setInterval(() => {
+            setContainerWidth(`0px`);
+            if(interests.length-1 === visibleIndex){
+                setVisibleIndex(0);
+            }else{
+                setVisibleIndex(visibleIndex+1);
+            }
+        }, 2000);
+        innerInterval = setInterval(() => {
+            setContainerWidth(`${0.45*(interests[visibleIndex].length)}em`)
+        },1000);
+        return ()=>{
+            clearInterval(innerInterval);
+            clearInterval(interval);
+        }
+    },[interests]);
+
     return (
         <div className="intro">
             <div className={IntroViewCSS.middleBannerSection}>
@@ -16,10 +39,10 @@ const Intro = () => {
                     <span className={IntroViewCSS.introView}>Rishabh Dangwal</span>
                     <br/>
                     <span className = "iama">I am </span> 
-                    <span className = {IntroViewCSS.wrapper}>
+                    <span className = {IntroViewCSS.wrapper} style ={{width:containerWidth}}>
                         {
-                            interests.map((interest) => (
-                                <b>{interest}</b>
+                            interests.map((interest,index) => (
+                                <b className = {classnames({[`${IntroViewCSS.isHidden}`]:(index !== visibleIndex)},{[`${IntroViewCSS.isVisible}`]:(index ===visibleIndex)})} >{interest}</b>
                             ))
                         }
                     </span>
