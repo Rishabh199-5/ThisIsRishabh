381ab5c2ff92c5f6a9399106ca1e1c1e83272c0c
Rishabh <rish@adobe.com>
2022-01-03 12:58:51 +0530

    using intersection observer to identify whether skill is in view port

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -6,23 +6,42 @@ import dwnldDoneImage from '../assets/dwnlddone.gif';
 const Skills = () => {
   const skills = [{ name: "html", rate: "90%" }, { name: "css", rate: "85%" }, { name: "angular", rate: "85%" }, { name: "reactjs", rate: "90%" }, { name: "javascript", rate: "75%" }, { name: "Design", rate: "60%" }, { name: "Data structures", rate: "75%" }, { name: "Problem Solving", rate: "75%" }];
   const [currImageUrl, setCurrImgUrl] = useState(downloadImage);
-  const ref = useRef();
+  const containerRef = useRef(null);
+  const ref = useRef(null);
   const imageOnClickHandler = () => {
     setCurrImgUrl(dwnldDoneImage);
   }
-
-  useEffect(() => {
-    setTimeout(() => {
+  const callBackFn = (entries) => {
+    const [ entry ] = entries;
+    if(entry.isIntersecting){
       if (ref && ref.current) {
         Array.prototype.forEach.call(ref.current.children, (child, index) => {
           child.firstChild.style.maxWidth = skills[index].rate;
         });
       }
-    }, 1000);
+    }
+  }
+
+
+  useEffect(() => {
+    let options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+    
+    let observer = new IntersectionObserver(callBackFn, options);
+    if(containerRef.current){
+      observer.observe(containerRef.current);
+    }
+    return () => {
+      if(containerRef.current){
+        observer.unobserve(containerRef.current)
+      }
+    }
   }, [])
 
   return (
-    <div className={SkillsCSS.wrapper}>
+    <div className={SkillsCSS.wrapper} ref= {containerRef}>
       <div className={SkillsCSS.leftPnl}>
         <p className={SkillsCSS.heading}>Download my Resume</p>
         <div className={SkillsCSS.imageWrapper}>
