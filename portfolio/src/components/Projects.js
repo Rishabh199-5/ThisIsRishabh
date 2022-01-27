7eb13c06250f9ab27c9f480bcfda61cf51c5be13
Rishabh <rish@adobe.com>
2022-01-27 23:30:39 +0530

    adding projects content and correcting styling

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -16,14 +16,14 @@ const Projects = () => {
     const animStr = (i) => `${ProjectCSS.fadeIn} ${duration}ms ease-out ${delay * i}ms forwards`;
 
     const projects = [
-        { title: "Real Time Application for Collaborative Editing", tech: "js/angular/nodejs" , filters: [0,1,3,5],imgUrl: realTimeImg},
-        { title: "Admissions", tech: "js/angular", filters: [0,1,2] ,imgUrl: admissionImg},
-        { title: "Point of Sale", tech: "angular" , filters: [0,1] ,imgUrl: posImg},
-        { title: "Ecommerce Applications", tech: "js/angular", filters: [0,1,2], imgUrl: ecommerceImg},
-        { title: "Websites", tech: "angular" , filters: [0,1],imgUrl: websitesImg},
-        { title: "Edit PDF on Web", tech: "reactjs/c++", filters: [0,4,5],imgUrl: pdfImg},
-        { title: "UnEmbedded Fonts in PDF", tech: "js" , filters: [0,2],imgUrl: pdfImg},
-        { title: "Acroforms in PDF", tech: "reactjs" ,filters: [0,5],imgUrl: pdfImg},
+        { title: "Real Time Application for Collaborative Editing", tech: "js/angular/nodejs" , filters: [0,1,3,5],imgUrl: realTimeImg , content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"},
+        { title: "Admissions", tech: "js/angular", filters: [0,1,2] ,imgUrl: admissionImg, content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"},
+        { title: "Point of Sale", tech: "angular" , filters: [0,1] ,imgUrl: posImg ,content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"},
+        { title: "Ecommerce Applications", tech: "js/angular", filters: [0,1,2], imgUrl: ecommerceImg, content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"},
+        { title: "Websites", tech: "angular" , filters: [0,1],imgUrl: websitesImg, content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"},
+        { title: "Edit PDF on Web", tech: "reactjs/c++", filters: [0,4,5],imgUrl: pdfImg, content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"},
+        { title: "UnEmbedded Fonts in PDF", tech: "js" , filters: [0,2],imgUrl: pdfImg, content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"},
+        { title: "Acroforms in PDF", tech: "reactjs" ,filters: [0,5],imgUrl: pdfImg, content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"},
     ];
     const [projectsList, setProjectsList] = useState(projects);
     const filters = [
@@ -65,6 +65,7 @@ const Projects = () => {
                             <div className={ProjectCSS.hoverContent}>
                                 <div className={ProjectCSS.text}>{project.title}</div>
                                 <div className={ProjectCSS.techHighLight}>{project.tech}</div>
+                                <div className={ProjectCSS.content}>{project.content}</div>
                             </div>
                         </div>
                 ))
